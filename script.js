94783396bde00b2aae5b69f333630dae81242591
Zeeshan Nawaz <83117864+zeeshanmnawaz@users.noreply.github.com>
2022-06-11 22:01:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function Number(num1,num2,sign){
+    if (sign=="+"){
+        return num1+num2;
+    }
+    else if (sign=="-"){
+        return num1-num2;
+    }
+    else if (sign=="*"){
+        return num1*num2;
+    }
+    else if (sign=="/"){
+        return num1/num2;
+    }
+    else{
+        console.log("please enter a valid operator")
+    }
+
+}
